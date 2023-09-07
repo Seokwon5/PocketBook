fb0f0cc9f5af5bbc4c4d7bcf8a0fe2d1bfe0f045
Seokwon5 <tjrdnjs5@gmail.com>
2023-09-07 16:11:10 +0900

    읽는중인 데이터 홈화면에 업데이트

---
+++ PocketBook/Scene/HomeView/CurrentBookCollectionViewCell.swift
@@ -0,0 +1,73 @@
+//
+//  CurrentBookCollectionViewCell.swift
+//  PocketBook
+//
+//  Created by 이석원 on 2023/09/02.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+class CurrentBookCollectionViewCell: UICollectionViewCell {
+    
+    
+    lazy var currentImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemBackground
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    lazy var currentBookLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.text = "책 제목"
+        label.font = .systemFont(ofSize: 16.0, weight: .semibold)
+        label.numberOfLines = 2
+        label.lineBreakMode = .byTruncatingTail
+         
+        return label
+    }()
+    private lazy var currentBookIndex: UILabel = {
+        let label = UILabel()
+        
+        label.text = "책갈피"
+        label.font = .systemFont(ofSize: 14.0, weight: .medium)
+         
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        [currentImageView, currentBookLabel, currentBookIndex].forEach { addSubview($0) }
+        
+        currentImageView.snp.makeConstraints{
+            $0.leading.equalToSuperview()
+            $0.top.equalToSuperview().inset(16.0)
+            $0.width.equalTo(120.0)
+            $0.height.equalTo(200.0)
+        }
+        
+        currentBookLabel.snp.makeConstraints{
+            $0.top.equalTo(currentImageView.snp.bottom).offset(4.0)
+            $0.leading.equalTo(currentImageView.snp.leading)
+            $0.width.equalTo(currentImageView.snp.width)
+        }
+        
+        currentBookIndex.snp.makeConstraints{
+            $0.top.equalTo(currentBookLabel.snp.bottom)
+            $0.leading.equalTo(currentBookLabel.snp.leading)
+        }
+    }
+}
